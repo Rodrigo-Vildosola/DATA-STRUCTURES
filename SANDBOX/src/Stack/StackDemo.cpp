d5e16d7eac1ae714d1b04e721e8e407461ec735d
Rodrigo-Vildosola <rjvildosola@miuandes.cl>
2024-07-09 01:25:19 -0400

    Hash templates

--- SANDBOX/src/Stack/StackDemo.cpp
+++ SANDBOX/src/Stack/StackDemo.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <string>
+#include <DTypes.h>
 #include <Structs.h>
 
 
@@ -7,30 +8,6 @@ namespace STRUCTS {
 
 }
 
-#include <iostream>
-#include <string>
-
-class Person {
-public:
-    std::string name;
-    int age;
-
-    Person() : name(""), age(0) {}
-    Person(std::string name, int age) : name(name), age(age) {}
-
-    bool operator==(const Person& other) const {
-        return name == other.name && age == other.age;
-    }
-
-    bool operator!=(const Person& other) const {
-        return !(*this == other);
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Person& person) {
-        os << person.name << " (" << person.age << ")";
-        return os;
-    }
-};
 
 
 
